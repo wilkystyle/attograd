1b273b3abcadd71e8b8b9682f0a941623933accd
Mike Wilkerson <wilkystyle@gmail.com>
2026-01-29 21:38:58 -0500

    Add a simple test to confirm forward pass operations

---
+++ tests/test_karthy_micrograd.py
@@ -0,0 +1,27 @@
+from attograd import Node
+
+
+def test_karpathy_micrograd_readme():
+    """Forward pass operations to confirm we get the same values as Micrograd README.
+
+    See https://github.com/karpathy/micrograd/blob/c911406e5ace8742e5841a7e0df113ecb5d54685/README.md#example-usage
+    """
+    a = Node(-4.0)
+    b = Node(2.0)
+
+    c = a + b
+    d = a * b + b**3
+
+    c += c + 1
+    c += 1 + c + (-a)
+
+    d += d * 2 + (b + a).relu()
+    d += 3 * d + (b - a).relu()
+
+    e = c - d
+    f = e**2
+
+    g = f / 2.0
+    g += 10.0 / f
+
+    assert g.value == 24.70408163265306
